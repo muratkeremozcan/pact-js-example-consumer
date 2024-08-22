4ee661e248cf47b6babd957fcc2a8347ed08eca7
Murat Ozcan <muratkerem@gmail.com>
2024-08-22 11:44:26 -0500

    ch6 working

--- provider/provider-contract.spec.js
+++ provider/provider-contract.spec.js
@@ -1,19 +1,42 @@
 const { Verifier } = require('@pact-foundation/pact')
-const { movies } = require('./provider')
+const { movies, server, importData } = require('./provider')
 
-// 1) Run the provider service: npm run start:provider
+// 1) Run the provider service
 // 2) Setup the provider verifier options
 // 3) Write & execute the provider contract test
+
+// TODO: Address the current limitation with data initialization.
+// The current setup requires starting the server and importing the data within the test itself,
+// ensuring that the stateHandlers can access the necessary state.
+// This approach is not ideal  because it tightly couples data setup with test execution,
+// leading to potential issues with test isolation and parallelism.
+
+// Future improvements:
+// - Separate the data layer, possibly using SQLite, to manage state independently
+// - Add a service client to interact with the service / database (refashion movies.js?)
+// - Enhance the workflow by starting and stopping the server via package.json scripts, decoupling it from the test file.
+
+const port = '3001'
+const app = server.listen(port, () =>
+  console.log(`Listening on port ${port}...`)
+)
+
+importData()
 
 // 2) Setup the provider verifier options
 const options = {
   provider: 'MoviesAPI',
-  providerBaseUrl: `http://localhost:3001`,
+  providerBaseUrl: `http://localhost:${port}`,
+  publishVerificationResult: true,
   pactBrokerUrl: process.env.PACT_BROKER_BASE_URL,
   pactBrokerToken: process.env.PACT_BROKER_TOKEN,
-  providerVersion: '1.0.0',
-  publishVerificationResult: true,
-  consumerVersionTags: ['main'], // an array of consumer versions to verify
+  providerVersion: process.env.GITHUB_SHA,
+  providerVersionBranch: process.env.GITHUB_BRANCH,
+  consumerVersionSelectors: [
+    { mainBranch: true },
+    { matchingBranch: true },
+    { deployedOrReleased: true }
+  ],
   // PROVIDER STATES: we can simulate certain states of the api (like an empty or non-empty db)
   // in order to cover different scenarios
   // the state could have many more variables; it is a good practice to represent it as an object
@@ -37,6 +60,10 @@ const options = {
 const verifier = new Verifier(options)
 
 describe('Pact Verification', () => {
+  afterAll(() => {
+    app.close()
+  })
+
   test('should validate the expectations of movie-consumer', () => {
     // 3) Write & execute the provider contract test (you have to return)
     return verifier.verifyProvider().then((output) => {
