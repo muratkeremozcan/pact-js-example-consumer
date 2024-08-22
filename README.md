0acbe9a5b8da682741c82e83805a89899a961562
Murat Ozcan <muratkerem@gmail.com>
2024-08-22 06:35:00 -0500

    ch6 until setting up state handler

--- README.md
+++ README.md
@@ -1,30 +1,47 @@
 # PactJS Contract Testing Example
 
-An example test framework using Pact-js to validate contract testing between consumer and provider. The application that we are testing is a simple movies API which returns a list of movies.
+An example test framework using Pact-js to validate contract testing between consumer and provider. The original example is from the [repo](https://github.com/mdcruz/pact-js-example) and book [Contract Testing in Action](https://www.manning.com/books/contract-testing-in-action).
 
-## Running the Movies API locally
+### Pact Broker setup
 
-Install dependencies
-`npm i`
+You need a Pact broker, and this example uses the free trial for [Pactflow](https://pactflow.io/try-for-free/).
 
-Run the movies API
-`npm run start:provider`
-
-## Running the tests
-
-I am using [Pactflow](https://pactflow.io/) as my broker. To use Pactflow , register for their free developer plan and export your Pactflow Broker URL and API token:
+Create a `.env` file in the fashion of the existing `.env.example` file.
 
 ```
-export PACT_BROKER_URL=<PACT_BROKER_URL here>
-export PACT_API_TOKEN=<API_TOKEN here>
+PACT_BROKER_TOKEN=***********
+PACT_BROKER_BASE_URL=https://yourownorg.pactflow.io
 ```
 
-Run the consumer tests:
-`npm run test:consumer`
+### Consumer side
 
-Publish the contract to your pact broker:
-`npm run publish:pact`
+The consumer is any type API client, including web apps.
 
-Run the provider tests
-`npm run start:provider`
-`npm run test:provider`
+Run the tests and create the contract / pact /json-file:
+
+```bash
+npm run test:consumer 
+```
+
+The contract and the broker are all that binds the Consumer and the Provider. 
+
+Therefore we need a way to upload the contract to the broker:
+
+```bash
+npm run publish:pact # uploads the contract to the broker
+```
+
+### Provider side 
+
+The consumer is any type of API server. As a requirement for contract testing we need to be able to run the provider app locally. Express.js is used in the example.
+
+```bash
+npm run start:provider # runs the provider app
+```
+
+Run the provider contract tests
+
+```bash
+npm run test:provider # Verifies the contract from the consumer
+```
+
--- consumer-web/consumer-contract.spec.js
+++ consumer-web/consumer-contract.spec.js
@@ -1,6 +1,10 @@
 const path = require('path')
-const { fetchMovies } = require('./consumer')
+const { fetchMovies, fetchSingleMovie } = require('./consumer')
 const { PactV3, MatchersV3 } = require('@pact-foundation/pact')
+
+// full list of matchers:
+// https://docs.pact.io/implementation_guides/javascript/docs/matching#v3-matching-rules
+const { eachLike, integer, string } = MatchersV3
 
 // 1) Setup the mock provider for the consumer
 // 2) Register the consumer's expectations against the (mock) provider
@@ -14,12 +18,12 @@ const provider = new PactV3({
   provider: 'MoviesAPI'
 })
 
-// loose matching: the consumer should care more about the shape of the data
-const EXPECTED_BODY = { id: 1, name: 'My movie', year: 1999 }
-
 describe('Movies API', () => {
   describe('When a GET request is made to /movies', () => {
     test('it should return all movies', async () => {
+      // loose matching: the consumer should care more about the shape of the data
+      const EXPECTED_BODY = { id: 1, name: 'My movie', year: 1999 }
+
       // 2) Register the consumer's expectations against the (mock) provider
       provider
         .uponReceiving('a request to get all movies')
@@ -29,7 +33,7 @@ describe('Movies API', () => {
         })
         .willRespondWith({
           status: 200,
-          body: MatchersV3.eachLike(EXPECTED_BODY)
+          body: eachLike(EXPECTED_BODY)
         })
 
       // 3) Call the consumer against the mock provider
@@ -37,6 +41,39 @@ describe('Movies API', () => {
         const movies = await fetchMovies(mockProvider.url)
         // 4) Verify the consumer test and generate the contract
         expect(movies[0]).toEqual(EXPECTED_BODY)
+      })
+    })
+  })
+
+  // PROVIDER STATES: we can simulate certain states of the api (like an empty or non-empty db)
+  // in order to cover different scenarios
+
+  describe('When a GET request is made to a specific movie ID', () => {
+    test('it should return a specific movie', async () => {
+      const testId = 100
+      const EXPECTED_BODY = { id: testId, name: 'My movie', year: 1999 }
+
+      provider
+        // the state could have many more variables; it is a good practice to represent it as an object
+        // note that the same state name is used on the provider side
+        .given(`Has a movie with a specific ID`, { id: testId })
+        .uponReceiving('a request to a specific movie')
+        .withRequest({
+          method: 'GET',
+          path: `/movie/${testId}`
+        })
+        .willRespondWith({
+          status: 200,
+          body: {
+            id: integer(testId),
+            name: string(EXPECTED_BODY.name),
+            year: integer(EXPECTED_BODY.year)
+          }
+        })
+
+      await provider.executeTest(async (mockProvider) => {
+        const movie = await fetchSingleMovie(mockProvider.url, testId)
+        expect(movie).toEqual(EXPECTED_BODY)
       })
     })
   })
