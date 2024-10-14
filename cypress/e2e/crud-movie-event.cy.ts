6780230c3202f1d7e438921e1352470db2b6fc87
Murat Ozcan <muratkerem@gmail.com>
2024-10-14 07:11:04 -0500

    feat: kafka part 2

---
+++ cypress/e2e/crud-movie-event.cy.ts
@@ -0,0 +1,114 @@
+import '@cypress/skip-test/support'
+
+import type { Movie } from '../../src/consumer'
+import { generateMovie } from '../support/factories'
+import spok from 'cy-spok'
+import { retryableBefore } from '../support/retryable-before'
+import { parseKafkaEvent } from '../support/parse-kafka-event'
+import { recurse } from 'cypress-recurse'
+
+describe('CRUD movie', () => {
+  const movie = generateMovie()
+  const updatedMovie = { name: 'Updated Name', year: 2000 }
+  const movieProps: Omit<Movie, 'id'> = {
+    name: spok.string,
+    year: spok.number
+  }
+
+  retryableBefore(() => {
+    cy.exec(
+      `curl -s -o /dev/null -w "%{http_code}" ${Cypress.env('KAFKA_UI_URL')}`,
+      {
+        failOnNonZeroExit: false
+      }
+    ).then((res) => {
+      cy.log('**npm run kafka:start at the server to enable this test**')
+      cy.skipOn(res.stdout !== '200')
+    })
+
+    cy.api({
+      method: 'GET',
+      url: '/'
+    })
+      .its('body.message')
+      .should('eq', 'Server is running')
+  })
+
+  it('should crud', () => {
+    cy.addMovie(movie)
+      .should(spok({ data: movieProps, status: 200 }))
+      .print()
+      .its('data.id')
+      .then((id) => {
+        recurse(
+          () => parseKafkaEvent(id, 'movie-created'),
+          spok([
+            {
+              id,
+              name: movie.name,
+              year: movie.year,
+              topic: 'movie-created'
+            }
+          ])
+        )
+
+        cy.getAllMovies().should(spok(spok.array)).findOne({ name: movie.name })
+
+        cy.getMovieById(id)
+          .its('data')
+          .should(
+            spok({
+              ...movieProps,
+              id
+            })
+          )
+          .its('name')
+          .then((name) => {
+            cy.getMovieByName(name)
+              .its('data')
+              .should(
+                spok({
+                  ...movieProps,
+                  id
+                })
+              )
+          })
+
+        cy.updateMovie(id, updatedMovie).should(
+          spok({
+            data: {
+              id,
+              name: updatedMovie.name,
+              year: updatedMovie.year
+            },
+            status: 200
+          })
+        )
+        recurse(
+          () => parseKafkaEvent(id, 'movie-updated'),
+          spok([
+            {
+              id,
+              name: updatedMovie.name,
+              year: updatedMovie.year,
+              topic: 'movie-updated'
+            }
+          ])
+        )
+
+        cy.deleteMovie(id)
+        recurse(
+          () => parseKafkaEvent(id, 'movie-deleted'),
+          spok([
+            {
+              id,
+              name: updatedMovie.name,
+              year: updatedMovie.year,
+              topic: 'movie-deleted'
+            }
+          ])
+        )
+        cy.getAllMovies().findOne({ name: movie.name }).should('not.exist')
+      })
+  })
+})
---
+++ cypress/support/parse-kafka-event.ts
@@ -0,0 +1,41 @@
+import type { Movie } from '../../src/consumer'
+import { logFilePath } from '../../src/events/log-file-path'
+import type { MovieAction } from '../../src/events/movie-event-types'
+
+type Entry = Movie & { topic: string }
+
+/**
+ * Curried filter function to filter by topic and movieId
+ *
+ * @param {number} movieId - The ID of the movie to filter by.
+ * @param {string} topic - The Kafka topic to filter by.
+ * @returns {(entries: Array<ReturnType<typeof reshape>>) => Array} - A function that filters entries based on the topic and movieId.
+ */
+const filterByTopicAndId =
+  (movieId: number, topic: string) => (entries: Entry[]) =>
+    entries.filter((entry: Entry) => {
+      console.log(topic)
+      console.log(entry.topic)
+      return entry.topic === topic && entry.id === movieId
+    })
+
+/**
+ * Parses the Kafka event log file and filters events based on the topic and movieId.
+ *
+ * @param {number} movieId - The ID of the movie to filter for.
+ * @param {MovieAction} topic - The Kafka topic to filter by.
+ * @param {string} [filePath=logFilePath] - Optional file path for the Kafka event log file.
+ * @returns {Cypress.Chainable} - A Cypress chainable that resolves to the first matching event.
+ */
+export const parseKafkaEvent = (
+  movieId: number,
+  topic: `movie-${MovieAction}`,
+  filePath = logFilePath
+) =>
+  cy
+    .print('parsing Kafka events..')
+    .readFile(filePath)
+    .invoke('trim')
+    .invoke('split', '\n')
+    .map(JSON.parse)
+    .apply(filterByTopicAndId(movieId, topic))
