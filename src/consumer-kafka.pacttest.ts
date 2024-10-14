9247ff355194c99a990845e69534b34e6323cfc0
Murat Ozcan <muratkerem@gmail.com>
2024-10-14 09:06:34 -0500

    test: kafka pact test

---
+++ src/consumer-kafka.pacttest.ts
@@ -0,0 +1,83 @@
+import {
+  MessageConsumerPact,
+  Matchers,
+  asynchronousBodyHandler
+} from '@pact-foundation/pact'
+import { consumeMovieEvents } from '../src/events/movie-events'
+import path from 'node:path'
+
+const { like, eachLike, term } = Matchers
+
+describe('Kafka Movie Event Consumer', () => {
+  const messagePact = new MessageConsumerPact({
+    dir: path.resolve(process.cwd(), 'pacts/messages'), // we save it in a different location than the contract test
+    consumer: 'WebConsumer',
+    provider: 'MoviesAPI'
+    // logLevel: 'debug'
+  })
+
+  const messages = eachLike({
+    key: like('1'),
+    value: JSON.stringify({
+      id: like(1),
+      name: like('Inception'),
+      year: like(2010)
+    })
+  })
+
+  // generate: will be returned during mock consumer testing to simulate a valid response.
+  // matcher: will be used to ensure that the provider returns a value that fits the expected pattern
+  // when the contract is verified on the provider side.
+  const matcher = '^movie-(created|updated|deleted)$' // The valid patterns for the event types
+
+  // our consumeMovieEvents has some console.logs which we don't need during tests
+  beforeAll(() => {
+    jest.spyOn(console, 'log').mockImplementation(() => {})
+    jest.spyOn(console, 'error').mockImplementation(() => {})
+  })
+  afterAll(() => {
+    jest.restoreAllMocks()
+  })
+
+  it('should receive a movie-created event from Kafka', async () => {
+    await messagePact
+      .given('No movies exist')
+      .expectsToReceive('a movie-created event')
+      .withContent({
+        topic: term({ generate: 'movie-created', matcher }),
+        messages
+      })
+      .withMetadata({
+        contentType: 'application/json'
+      })
+      .verify(asynchronousBodyHandler(consumeMovieEvents))
+  })
+
+  it('should receive a movie-updated event from Kafka', async () => {
+    await messagePact
+      .given('An existing movie exists')
+      .expectsToReceive('a movie-updated event')
+      .withContent({
+        topic: term({ generate: 'movie-updated', matcher }),
+        messages
+      })
+      .withMetadata({
+        contentType: 'application/json'
+      })
+      .verify(asynchronousBodyHandler(consumeMovieEvents))
+  })
+
+  it('should receive a movie-deleted event from Kafka', async () => {
+    await messagePact
+      .given('An existing movie exists')
+      .expectsToReceive('a movie-deleted event')
+      .withContent({
+        topic: term({ generate: 'movie-deleted', matcher }),
+        messages
+      })
+      .withMetadata({
+        contentType: 'application/json'
+      })
+      .verify(asynchronousBodyHandler(consumeMovieEvents))
+  })
+})
