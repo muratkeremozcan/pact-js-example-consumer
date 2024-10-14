e8daa5ef396488291c5d8c86f424ccdc17404273
Murat Ozcan <muratkerem@gmail.com>
2024-10-14 13:56:20 -0500

    test: kafka fixed scripts

--- scripts/record-consumer-deployment.sh
+++ scripts/record-consumer-deployment.sh
@@ -3,11 +3,16 @@
 # Load environment variables
 . ./scripts/env-setup.sh
 
-# Only run if on the main branch
+# Only record deployment if on the main branch
 if [ "$GITHUB_BRANCH" = "main" ]; then
+  # Record deployment for WebConsumer
   pact-broker record-deployment \
       --pacticipant WebConsumer \
       --version $GITHUB_SHA \
+      --environment $npm_config_env
+  
+  # Record deployment for WebConsumer-event-consumer
+  pact-broker record-deployment \
       --pacticipant WebConsumer-event-consumer \
       --version $GITHUB_SHA \
       --environment $npm_config_env
