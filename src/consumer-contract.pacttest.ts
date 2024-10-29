8aa6e8a635f8f139f4a1f6c18ffd90e07870109e
Murat Ozcan <muratkerem@gmail.com>
2024-10-29 14:39:37 -0500

    trigger

--- src/consumer-contract.pacttest.ts
+++ src/consumer-contract.pacttest.ts
@@ -55,7 +55,7 @@ describe('Movies API', () => {
       await pact
         .addInteraction()
         .given(stateName, stateParams)
-        .uponReceiving('a request to get all movies TRIGGE')
+        .uponReceiving('a request to get all movies TRIGG')
         .withRequest('GET', '/movies')
         .willRespondWith(
           200,
