d67b62c88037d836068cb96b75145eeed7c29d00
Murat Ozcan <muratkerem@gmail.com>
2024-11-06 05:17:52 -0600

    webhook from PR body

--- scripts/can-i-deploy-consumer.sh
+++ scripts/can-i-deploy-consumer.sh
@@ -11,19 +11,25 @@ RETRY_WHILE_UNKNOWN=4   # Number of retries
 RETRY_INTERVAL=80       # Seconds between retries
 
 # Check if WebConsumer can be deployed
-pact-broker can-i-deploy \
-    --pacticipant WebConsumer \
-    --version=$GITHUB_SHA \
-    --to-environment dev \
-    --retry-while-unknown=$RETRY_WHILE_UNKNOWN \
-    --retry-interval=$RETRY_INTERVAL \
-    --verbose
+PACT_BREAKING_CHANGE=${PACT_BREAKING_CHANGE:-false}
 
-# Check if WebConsumer-event-consumer can be deployed
-pact-broker can-i-deploy \
-    --pacticipant WebConsumer-event-consumer \
-    --version=$GITHUB_SHA \
-    --to-environment dev \
-    --retry-while-unknown=$RETRY_WHILE_UNKNOWN \
-    --retry-interval=$RETRY_INTERVAL \
-    --verbose
+if [ "$PACT_BREAKING_CHANGE" = "true" ]; then
+  echo "Breaking change detected. Running can-i-deploy only against matching branch."
+  pact-broker can-i-deploy \
+      --pacticipant WebConsumer \
+      --version=$GITHUB_SHA \
+      --to-environment dev \
+      --retry-while-unknown=$RETRY_WHILE_UNKNOWN \
+      --retry-interval=$RETRY_INTERVAL \
+      --consumer-version-selector matchingBranch \
+      --verbose
+else
+  echo "No breaking change. Running can-i-deploy against matching branch + main + deployed."
+  pact-broker can-i-deploy \
+      --pacticipant WebConsumer \
+      --version=$GITHUB_SHA \
+      --to-environment dev \
+      --retry-while-unknown=$RETRY_WHILE_UNKNOWN \
+      --retry-interval=$RETRY_INTERVAL \
+      --verbose
+fi
