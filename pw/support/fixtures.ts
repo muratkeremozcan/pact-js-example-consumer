878830fc0be9b47c0d2216878d015d4d329a8c7e
muratkeremozcan <muratkerem@gmail.com>
2024-12-26 07:38:22 -0600

    test: pw refinements 1

--- pw/support/fixtures.ts
+++ pw/support/fixtures.ts
@@ -1,5 +1,5 @@
 import { test as base, mergeTests } from '@playwright/test'
-import { test as apiRequestFixture } from './helpers/api-request-fixture'
+import { test as apiRequestFixture } from './fixtures/api-request-fixture'
 
 // Merge the fixtures
 const test = mergeTests(apiRequestFixture) // Add new fixtures as arguments
