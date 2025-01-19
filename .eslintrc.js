9ab099d5886a0df2b6829b06f1acb93b84aeabe3
muratkeremozcan <muratkerem@gmail.com>
2025-01-19 08:54:56 -0600

    test:eslint await thenable

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,6 +33,7 @@ module.exports = {
   rules: {
     '@typescript-eslint/consistent-type-imports': 'error',
     '@typescript-eslint/consistent-type-exports': 'error',
+    '@typescript-eslint/await-thenable': 'error',
     '@typescript-eslint/no-floating-promises': 'error',
     'no-only-tests/no-only-tests': 'error',
     '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
